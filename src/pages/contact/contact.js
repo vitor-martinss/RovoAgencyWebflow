35a701f757161297e4770f322653a7f6953573e0
Vitor Martins <vitormartinsweb@gmail.com>
2023-02-16 23:01:57 +0000

    feat: modals and header

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,34 +1,108 @@
-const btnOpenModal = document.querySelectorAll('.rovo-list__item-content')
-const btnCloseModal = document.querySelectorAll('.rovo-list-modal__item-close')
-const overlayCloseModal = document.querySelectorAll('.rovo-list-modal--overlay')
+if (document.getElementById('rovo-slider-numbers')) {
+  const swiper = new Swiper("#rovo-slider-numbers", {
+    grabCursor: true,
+    simulateTouch: true,
+    effect: "creative",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: false,
+    watchSlidesProgress: true,
+    shortSwipes: false,
+    loop: true,
+    pagination: {
+      el: document.querySelector('.rovo-slider-numbers .swiper-pagination'),
+      clickable: true,
+    },
 
-const handleOpenModal = (element) => {
-  element.forEach((el) => {
-    el.addEventListener('click', () => {
-      el.parentElement.classList.add('modal-open');
-      document.body.classList.add('overflow-hidden');
-      document.querySelector('html').classList.add('overflow-hidden');
-      const clonePills = el.lastChild.cloneNode(true)
-      el.parentElement.querySelector('.rovo-js-append-pills').append(clonePills)
-    })
+    creativeEffect: {
+      limitProgress: 1,
+      prev: {
+        shadow: false,
+        translate: [0, 0, -0.5]
+      },
+      next: {
+        translate: ["100%", 0, 0],
+      },
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 1.5,
+        spaceBetween: 32,
+        creativeEffect: {
+          limitProgress: 2,
+          prev: {
+            shadow: false,
+            translate: [0, 0, -0.5]
+          },
+          next: {
+            translate: ["105%", 0, 0],
+          },
+        }
+      },
+      1024: {
+        slidesPerView: 1.8,
+        spaceBetween: 32,
+        slideToClickedSlide: true,
+        creativeEffect: {
+          limitProgress: 2,
+          prev: {
+            shadow: false,
+            translate: [0, 0, -0.5]
+          },
+          next: {
+            translate: ["105%", 0, 0],
+          },
+        }
+      },
+      1280: {
+        slidesPerView: 2.5,
+        spaceBetween: 32,
+        slideToClickedSlide: true,
+        creativeEffect: {
+          limitProgress: 2.5,
+          prev: {
+            shadow: false,
+            translate: [0, 0, -0.5]
+          },
+          next: {
+            translate: ["105%", 0, 0],
+          }
+        }
+      }
+    }
   })
 }
 
-const handleCloseModal = (element) => {
-  element.forEach((el) => {
-    el.addEventListener('click', () => {
-      el.closest('.modal-open').querySelector('.rovo-js-append-pills').lastChild.remove()
-      el.closest('.modal-open').classList.remove('modal-open');
-      document.body.classList.remove('overflow-hidden');
-      document.querySelector('html').classList.remove('overflow-hidden');
 
-    })
+if (document.getElementById('rovo-slider-cards')) {
+  const swiper = new Swiper("#rovo-slider-cards", {
+    grabCursor: true,
+    simulateTouch: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: false,
+    watchSlidesProgress: true,
+    loop: true,
+    pagination: {
+      el: document.querySelector('.rovo-slider-cards .swiper-pagination'),
+      clickable: true,
+    },
+
+    breakpoints: {
+      768: {
+        loop: true,
+        slidesPerView: 1.5,
+        spaceBetween: 32,
+      },
+      1024: {
+        loop: true,
+        slidesPerView: 1.5,
+        spaceBetween: 32,
+        slideToClickedSlide: true
+      }
+    }
   })
 }
-
-handleOpenModal(btnOpenModal)
-handleCloseModal(btnCloseModal)
-handleCloseModal(overlayCloseModal)
 
 if (window.innerWidth >= 768) {
   const footer = document.querySelector('footer');
@@ -36,8 +110,8 @@ if (window.innerWidth >= 768) {
   window.addEventListener('scroll', () => {
     const footerTop = footer.getBoundingClientRect().top;
 
-    if (footerTop < window.innerHeight) {
-      header.style.height = `calc(100vh - ${window.innerHeight - footerTop}px - 90px)`;
+    if (footerTop < (window.innerHeight + 60)) {
+      header.style.height = `calc(100vh - ${window.innerHeight - footerTop}px - 60px)`;
     } else {
       header.style.height = '100vh';
     }
@@ -52,12 +126,43 @@ if (window.innerWidth < 768) {
   headerButtonToggle.addEventListener('click', () => {
     navMobile.classList.toggle('open');
     document.body.classList.toggle('overflow-hidden');
+    document.querySelector('html').classList.toggle('overflow-hidden');
   })
 
   anchorItems.forEach((el) => {
     el.addEventListener('click', () => {
       navMobile.classList.remove('open');
       document.body.classList.toggle('overflow-hidden');
+      document.querySelector('html').classList.toggle('overflow-hidden');
     })
   })
 }
+
+const btnOpenModal = document.querySelectorAll('.rovo-list__item-content')
+const btnCloseModal = document.querySelectorAll('.rovo-list-modal__item-close')
+const overlayCloseModal = document.querySelectorAll('.rovo-list-modal--overlay')
+
+const handleOpenModal = (element) => {
+  element.forEach((el) => {
+    el.addEventListener('click', () => {
+      el.parentElement.classList.add('modal-open');
+      document.body.classList.add('overflow-hidden');
+      document.querySelector('html').classList.add('overflow-hidden');
+    })
+  })
+}
+
+
+const handleCloseModal = (element) => {
+  element.forEach((el) => {
+    el.addEventListener('click', () => {
+      el.closest('.rovo-list__item.modal-open').classList.remove('modal-open');
+      document.body.classList.remove('overflow-hidden');
+      document.querySelector('html').classList.remove('overflow-hidden');
+    })
+  })
+}
+
+handleOpenModal(btnOpenModal)
+handleCloseModal(btnCloseModal)
+handleCloseModal(overlayCloseModal)
