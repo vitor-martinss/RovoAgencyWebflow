751904f78ce35d3758b02471e5418930a0decb0b
vitor-martinss <vitormartinsweb@gmail.com>
2023-02-25 16:30:03 +0000

    fix: bug on header

--- src/methods/asideHeaderHeightOnScroll.js
+++ src/methods/asideHeaderHeightOnScroll.js
@@ -1,17 +1,28 @@
+const footer = document.querySelector("footer");
+const header = document.querySelector("header");
+
+function handleAsideHeaderHeightOnScroll() {
+  if (window.innerWidth >= 768) {
+    const footerTop = footer.getBoundingClientRect().top;
+
+    if (footerTop < window.innerHeight + 60) {
+      header.style.height = `calc(100vh - ${
+        window.innerHeight - footerTop
+      }px - 60px)`;
+    } else {
+      header.style.height = "100vh";
+    }
+  }
+}
+
+window.addEventListener("scroll", () => {
+  handleAsideHeaderHeightOnScroll();
+});
+
 window.addEventListener("resize", () => {
   if (window.innerWidth >= 768) {
-    const footer = document.querySelector("footer");
-    const header = document.querySelector("header");
-    window.addEventListener("scroll", () => {
-      const footerTop = footer.getBoundingClientRect().top;
-
-      if (footerTop < window.innerHeight + 60) {
-        header.style.height = `calc(100vh - ${
-          window.innerHeight - footerTop
-        }px - 60px)`;
-      } else {
-        header.style.height = "100vh";
-      }
-    });
+    handleAsideHeaderHeightOnScroll();
+  } else {
+    header.style.height = "4.375rem";
   }
 });
