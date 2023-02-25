34262f42a59f0d28b8a55ddc9a3e12256e069290
vitor-martinss <vitormartinsweb@gmail.com>
2023-02-25 16:07:56 +0000

    feat: watch resize on header desktop

--- src/methods/asideHeaderHeightOnScroll.js
+++ src/methods/asideHeaderHeightOnScroll.js
@@ -1,13 +1,17 @@
-if (window.innerWidth >= 768) {
-  const footer = document.querySelector('footer');
-  const header = document.querySelector('header');
-  window.addEventListener('scroll', () => {
-    const footerTop = footer.getBoundingClientRect().top;
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    const footer = document.querySelector("footer");
+    const header = document.querySelector("header");
+    window.addEventListener("scroll", () => {
+      const footerTop = footer.getBoundingClientRect().top;
 
-    if (footerTop < (window.innerHeight + 60)) {
-      header.style.height = `calc(100vh - ${window.innerHeight - footerTop}px - 60px)`;
-    } else {
-      header.style.height = '100vh';
-    }
-  })
-}
+      if (footerTop < window.innerHeight + 60) {
+        header.style.height = `calc(100vh - ${
+          window.innerHeight - footerTop
+        }px - 60px)`;
+      } else {
+        header.style.height = "100vh";
+      }
+    });
+  }
+});
