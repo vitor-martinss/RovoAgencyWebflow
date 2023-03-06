005848a4e32b431221d16ec7dd304328b61e7f93
vitor-martinss <vitormartinsweb@gmail.com>
2023-03-06 18:56:52 +0000

    refactor: js closure functions

--- src/methods/homeInitialAnimation.js
+++ src/methods/homeInitialAnimation.js
@@ -1,70 +1,82 @@
-function onStartAnimation() {
-  document
-    .querySelector(".rovo-home-animation")
-    .classList.add("rovo-home-animation--active");
-}
-
-function onCompleteAnimation() {
-  document
-    .querySelector(".rovo-home-animation")
-    .classList.remove("rovo-home-animation--active");
-}
-
-function homeAnimation() {
-  gsap.set(".rovo-reels-header", {
-    opacity: 0
-  });
-
-  const tl = gsap.timeline({
-    onStart: onStartAnimation(),
-    onComplete: onCompleteAnimation()
-  });
-  tl.to(".rovo-home-animation__image", {
-    y: 0,
-    delay: 0.5,
-    duration: 0.3
-  });
-  tl.to(".rovo-home-animation__image", {
-    y: -250,
-    delay: 1,
-    duration: 0.3
-  });
-  tl.to(
-    ".rovo-home-animation__heading",
-    {
-      y: 0,
-      duration: 0.3
-    },
-    ">-0.4"
-  );
-  tl.to(".rovo-home-animation__heading", {
-    y: -250,
-    delay: 1,
-    duration: 0.3
-  });
-  tl.to(
-    ".rovo-home-animation",
-    {
-      height: 0,
-      duration: 0.3
-    },
-    "<"
-  );
-  tl.to(".rovo-reels-header", {
-    opacity: 1,
-    duration: 0.2
-  });
-}
-
-function setSessionStorage() {
-  sessionStorage.setItem("homeAnimation", "true");
-}
-
-window.addEventListener("load", () => {
-  if (sessionStorage.getItem("homeAnimation") !== "true") {
-    homeAnimation();
-    setSessionStorage();
-  } else {
-    document.querySelector(".rovo-home-animation").remove();
+function homeInitialAnimation() {
+  const selectors = {
+    homeAnimationContainer: document.querySelector(".rovo-home-animation"),
+    homeHeader: ".rovo-reels-header",
+    homeAnimationImage: ".rovo-home-animation__image",
+    homeAnimationHeading: ".rovo-home-animation__heading",
+    homeAnimatedContainer: ".rovo-home-animation"
   }
-});
+
+  if (!selectors.homeAnimationContainer) {
+    return null;
+  }
+
+  function onStartAnimation() {
+    selectors.homeAnimationContainer
+      .classList.add("rovo-home-animation--active");
+  }
+
+  function onCompleteAnimation() {
+    selectors.homeAnimationContainer
+      .classList.remove("rovo-home-animation--active");
+  }
+
+  function homeAnimation() {
+    gsap.set(selectors.homeHeader, {
+      opacity: 0
+    });
+
+    const tl = gsap.timeline({
+      onStart: onStartAnimation(),
+      onComplete: onCompleteAnimation()
+    });
+    tl.to(selectors.homeAnimationImage, {
+      y: 0,
+      delay: 0.5,
+      duration: 0.3
+    });
+    tl.to(selectors.homeAnimationImage, {
+      y: -250,
+      delay: 1,
+      duration: 0.3
+    });
+    tl.to(
+      selectors.homeAnimationHeading, {
+        y: 0,
+        duration: 0.3
+      },
+      ">-0.4"
+    );
+    tl.to(selectors.homeAnimationHeading, {
+      y: -250,
+      delay: 1,
+      duration: 0.3
+    });
+    tl.to(
+      selectors.homeAnimatedContainer, {
+        height: 0,
+        duration: 0.3
+      },
+      "<"
+    );
+    tl.to(selectors.homeHeader, {
+      opacity: 1,
+      duration: 0.2
+    });
+  }
+
+  function setSessionStorage() {
+    sessionStorage.setItem("homeAnimation", "true");
+  }
+
+  window.addEventListener("load", () => {
+    if (sessionStorage.getItem("homeAnimation") !== "true") {
+      homeAnimation();
+      setSessionStorage();
+    } else {
+      selectors.homeAnimationContainer && selectors.homeAnimationContainer.remove();
+    }
+  });
+}
+
+homeInitialAnimation()
