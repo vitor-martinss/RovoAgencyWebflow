183fee2a7b8a8e71b622c4ebb793238e2587d43b
vitor-martinss <vitormartinsweb@gmail.com>
2023-03-11 17:49:59 +0000

    fix: sliders with swiper

--- src/methods/swiperCreativeSlider.js
+++ src/methods/swiperCreativeSlider.js
@@ -1,74 +1,79 @@
-if (document.getElementById('rovo-slider-numbers')) {
+if (document.getElementById("rovo-slider-numbers")) {
   const swiper = new Swiper("#rovo-slider-numbers", {
     grabCursor: true,
     simulateTouch: true,
     effect: "creative",
+    snapOnRelease: true,
     slidesPerView: 1,
     spaceBetween: 0,
     centeredSlides: false,
     watchSlidesProgress: true,
-    shortSwipes: false,
+    shortSwipes: true,
     loop: true,
+    draggable: true,
     pagination: {
-      el: document.querySelector('.rovo-slider-numbers .swiper-pagination'),
-      clickable: true,
+      el: document.querySelector(".rovo-slider-numbers .swiper-pagination"),
+      clickable: false
     },
-
     creativeEffect: {
       limitProgress: 1,
       prev: {
         shadow: false,
-        translate: [0, 0, -0.5]
+        translate: ["0%", 0, -0.5]
       },
       next: {
-        translate: ["100%", 0, 0],
-      },
+        translate: ["100%", 0, 0]
+      }
     },
     breakpoints: {
       768: {
         slidesPerView: 1.5,
+        allowSlidePrev: false,
         spaceBetween: 32,
         creativeEffect: {
-          limitProgress: 2,
+          limitProgress: 1,
           prev: {
             shadow: false,
-            translate: [0, 0, -0.5]
+            translate: ["0%", 0, -0.5]
           },
           next: {
-            translate: ["105%", 0, 0],
-          },
+            translate: ["105%", 0, 0]
+          }
         }
       },
-      1024: {
+      992: {
         slidesPerView: 1.8,
         spaceBetween: 32,
         slideToClickedSlide: true,
+        draggable: false,
+        simulateTouch: false,
         creativeEffect: {
-          limitProgress: 2,
+          limitProgress: 1,
           prev: {
             shadow: false,
-            translate: [0, 0, -0.5]
+            translate: ["0%", 0, -0.5]
           },
           next: {
-            translate: ["105%", 0, 0],
-          },
+            translate: ["105%", 0, 0]
+          }
         }
       },
       1280: {
         slidesPerView: 2.5,
         spaceBetween: 32,
         slideToClickedSlide: true,
+        draggable: false,
         creativeEffect: {
           limitProgress: 2.5,
           prev: {
             shadow: false,
-            translate: [0, 0, -0.5]
+            translate: ["0%", 0, -0.5]
           },
           next: {
-            translate: ["105%", 0, 0],
+            translate: ["105%", 0, 0]
           }
         }
       }
     }
-  })
+  });
 }
--- src/methods/swiperDefaultSlider.js
+++ src/methods/swiperDefaultSlider.js
@@ -1,21 +1,23 @@
-if (document.getElementById('rovo-slider-cards')) {
+if (document.getElementById("rovo-slider-cards")) {
   const swiper = new Swiper("#rovo-slider-cards", {
     grabCursor: true,
     simulateTouch: true,
     slidesPerView: 1,
     spaceBetween: 0,
+    snapOnRelease: true,
     centeredSlides: false,
     watchSlidesProgress: true,
+    shortSwipes: true,
     loop: true,
     pagination: {
-      el: document.querySelector('.rovo-slider-cards .swiper-pagination'),
-      clickable: true,
+      el: document.querySelector(".rovo-slider-cards .swiper-pagination"),
+      clickable: true
     },
 
     breakpoints: {
       768: {
         slidesPerView: 1.5,
-        spaceBetween: 32,
+        spaceBetween: 32
       },
       1024: {
         slidesPerView: 1.5,
@@ -23,5 +25,5 @@ if (document.getElementById('rovo-slider-cards')) {
         slideToClickedSlide: true
       }
     }
-  })
+  });
 }
